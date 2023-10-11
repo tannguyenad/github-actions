139dc8223c3f98ec806a13d5074e43ca4eeb39ef
Tan Nguyen <duytan1909@gmail.com>
2023-10-11 22:30:30 +0700

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react 12345/i);
+  const linkElement = screen.getByText(/learn react 1234/i);
   expect(linkElement).toBeInTheDocument();
 });
